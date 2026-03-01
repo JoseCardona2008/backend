1df9bc0b3dcda33af5549035e0db19fda795c6c0
Tatiana Suarez <yuleith07@gmail.com>
2026-02-28 19:22:13 -0500

    Model:Implementacion de models Role,Student,Subject

--- src/cesde/edu/ga/model/Student.java
+++ src/cesde/edu/ga/model/Student.java
@@ -1,4 +1,82 @@
-package cesde.edu.ga.model;
+package co.edu.cesde.ga.model;
 
 public class Student {
+
+    private Long userId;
+    private String code;
+    private String documentNumber;
+    private String firstName;
+    private String lastName;
+    private String status;
+    private String birthDate;
+
+    public Student() {
+    }
+
+    public Student(Long userId, String code, String documentNumber,
+                   String firstName, String lastName, String status, String birthDate) {
+        this.userId = userId;
+        this.code = code;
+        this.documentNumber = documentNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.status = status;
+        this.birthDate = birthDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 }
