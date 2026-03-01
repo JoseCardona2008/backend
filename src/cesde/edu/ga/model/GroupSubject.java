e85a53bfe73a9ff7f5811dc475ba077b76518f1a
juanjos373 <juanjosesaenzmontoya@gmail.com>
2026-02-28 19:15:20 -0500

    Model: Implementacion de models GroupSubjects, Period, Program

--- src/cesde/edu/ga/model/GroupSubject.java
+++ src/cesde/edu/ga/model/GroupSubject.java
@@ -1,4 +1,51 @@
 package cesde.edu.ga.model;
 
 public class GroupSubject {
+
+    private Long groupSubjectId;
+    private Long groupId;
+    private Long subjectId;
+    private Long teacherId;
+
+    public GroupSubject() {
+    }
+
+    public GroupSubject(Long groupSubjectId, Long groupId, Long subjectId, Long teacherId) {
+        this.groupSubjectId = groupSubjectId;
+        this.groupId = groupId;
+        this.subjectId = subjectId;
+        this.teacherId = teacherId;
+    }
+
+    public Long getGroupSubjectId() {
+        return groupSubjectId;
+    }
+
+    public void setGroupSubjectId(Long groupSubjectId) {
+        this.groupSubjectId = groupSubjectId;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    public Long getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
 }
--- src/cesde/edu/ga/model/Program.java
+++ src/cesde/edu/ga/model/Program.java
@@ -1,4 +1,41 @@
 package cesde.edu.ga.model;
 
 public class Program {
+
+    private Long programId;
+    private String code;
+    private String name;
+
+    public Program() {
+    }
+
+    public Program(Long programId, String code, String name) {
+        this.programId = programId;
+        this.code = code;
+        this.name = name;
+    }
+
+    public Long getProgramId() {
+        return programId;
+    }
+
+    public void setProgramId(Long programId) {
+        this.programId = programId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
